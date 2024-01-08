972e41c8047accd00a6b8efa2220f99941724cc2
Ivsmcrew <Ivsmcrew@gmail.com>
2024-01-08 22:28:11 +0300

    feat: create file structure for sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import AuthProvider from "./components/AuthProvider";
 import IvsmNavbar from "./UI/navbar/IvsmNavbar";
-import './styles/App.css';
+import IvsmSidebar from "./UI/sidebar/IvsmSidebar";
 import AppRouter from "./components/AppRouter";
 import Footer from "./UI/footer/Footer";
+import './styles/App.css';
                                                   
 function App() {
   return (
     <AuthProvider>
       <IvsmNavbar />
+      <IvsmSidebar />
       <AppRouter />
       <Footer />
     </AuthProvider>
---
+++ src/UI/sidebar/IvsmSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IvsmSidebar() {
+  return (
+    <div>
+      IvsmSidebar
+    </div>
+  )
+}
+
+export default IvsmSidebar
