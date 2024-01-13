f898309458e44eafa0cc6e2ebe4bb6fd6d628737
Ivsmcrew <Ivsmcrew@gmail.com>
2024-01-13 16:40:16 +0300

    feat: add redirect on main page after auth

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,14 +2,17 @@ import React, { useContext } from "react";
 import IvsmLoginForm from "../../components/IvsmLoginForm";
 import IvsmLoginQR from "../../components/IvsmLoginQR";
 import { UserAuthContext } from "../../context/context";
+import { useNavigate } from "react-router-dom";
 
 const Login = function() {
   const {isAuth, setIsAuth} = useContext(UserAuthContext)
+  const navigate = useNavigate();
 
   function login(event) {
     event.preventDefault();
     setIsAuth(true);
     localStorage.setItem('auth', 'true');
+    navigate('/');
   }
 
   return(
