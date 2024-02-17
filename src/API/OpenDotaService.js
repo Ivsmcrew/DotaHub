2809b4b41b203dca187692243df9f1afae74c942
Ivsmcrew <Ivsmcrew@gmail.com>
2024-02-17 20:51:43 +0300

    feat: implement basic table

--- src/API/OpenDotaService.js
+++ src/API/OpenDotaService.js
@@ -1,22 +1,37 @@
+import classes from "../pages/Heroes/Heroes.module.css";
+
 class OpenDotaService {
+  static gamesQuantity = 2000;
   static steamCDN1 = 'https://cdn.dota2.com';
   static steamCDN2 = 'https://steamcdn-a.akamaihd.net';
   static openDotaCDN = 'https://api.opendota.com/api';
 
+  //returns heroStats promise
   static async getHeroStats() {
     try {
-      const response = await fetch(this.openDotaCDN + '/heroStats');
-      return response
+      let response = await fetch(this.openDotaCDN + '/heroStats');
+      let heroStats = await response.json();
+      return heroStats
     } catch(err) {
-      console.log(err.message)
+      console.log('Error message: ' + err.message)
     }   
   }
 
+  //returns heroesDataArr promise
   static async getHeroesDataArr() {
     try {
-      // код получения данных через getHeroStats
+      const heroStatsArr = await this.getHeroStats();
+      const heroesDataArr = heroStatsArr.map((item) => {
+        return new Map([
+          ['heroImg', <img className={classes.heroIcon} src={this.steamCDN1 + item.img} alt="hero icon" />], 
+          ['pick', Math.round(item.pro_pick / this.gamesQuantity * 100)], 
+          ['ban', Math.round(item.pro_ban / this.gamesQuantity * 100)], 
+          ['win', Math.round((item.pro_win / item.pro_pick) * 100)],
+        ])
+      })
+      return heroesDataArr
     } catch(err) {
-      console.log(err.message)
+      console.log('Error message' + err.message)
     }
   }
 }
--- src/pages/Heroes/Heroes.jsx
+++ src/pages/Heroes/Heroes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OpenDotaService from "../../API/OpenDotaService";
 import classes from "./Heroes.module.css";
 import AuxiliaryNav from "../../components/AuxiliaryNav";
@@ -11,29 +11,17 @@ const Heroes = function() {
     {to: '/heroes/public', title: 'PUBLIC'},
     {to: '/heroes/turbo', title: 'TURBO'},
   ]
-  const headersArr = ['HERO', 'PRO PICK+BAN(%)', 'PRO BAN(%)', 'PRO WIN(%)'];
-  const heroesDataArr = [
-    new Map([
-      ['heroImg', <img className={classes.heroIcon} src={OpenDotaService.steamCDN1 + '/apps/dota2/images/dota_react/heroes/antimage.png?'} alt="hero icon" />], ['pick', 'pick%'], ['ban', 'ban%'], ['win', 'win%']
-    ]),
-    new Map([
-      ['heroImg', <img className={classes.heroIcon} src={OpenDotaService.steamCDN1 + '/apps/dota2/images/dota_react/heroes/antimage.png?'} alt="hero icon" />], ['pick', 'pick%'], ['ban', 'ban%'], ['win', 'win%']
-    ]),
-    new Map([
-      ['heroImg', <img className={classes.heroIcon} src={OpenDotaService.steamCDN1 + '/apps/dota2/images/dota_react/heroes/antimage.png?'} alt="hero icon" />], ['pick', 'pick%'], ['ban', 'ban%'], ['win', 'win%']
-    ]),
-    new Map([
-      ['heroImg', <img className={classes.heroIcon} src={OpenDotaService.steamCDN1 + '/apps/dota2/images/dota_react/heroes/antimage.png?'} alt="hero icon" />], ['pick', 'pick%'], ['ban', 'ban%'], ['win', 'win%']
-    ]),
-    new Map([
-      ['heroImg', <img className={classes.heroIcon} src={OpenDotaService.steamCDN1 + '/apps/dota2/images/dota_react/heroes/antimage.png?'} alt="hero icon" />], ['pick', 'pick%'], ['ban', 'ban%'], ['win', 'win%']
-    ])
-  ]
+  const headersArr = ['HERO', 'PRO PICK(%)', 'PRO BAN(%)', 'PRO WIN(%)'];
 
   //________________________________________________________STATES________________________________________
-
+  const [heroesDataArr, setHeroesDataArr] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      setHeroesDataArr(await OpenDotaService.getHeroesDataArr());
+    }
+    fetchData()
+  }, [])
  
-
   //______________________________________________________FUNCTIONS________________________________________
   
 
