42917242ef31ce740f9ebfb4daf27ba0fc7723e3
Ivsmcrew <Ivsmcrew@gmail.com>
2024-03-19 16:14:06 +0300

    feat: add right qr code

--- src/components/IvsmLoginQR.jsx
+++ src/components/IvsmLoginQR.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import qrPng from '../assets/images/qr.png';
+import qrPng from '../assets/images/qr-code.gif';
 
 const IvsmLoginQR = function() {
   function getQR() {
@@ -10,7 +10,7 @@ const IvsmLoginQR = function() {
     <div className="qr">
       <h4 className="h4">ENTER WITH QR-CODE</h4>
       <div className="qr__background">
-        <img src={getQR()} alt="Unique qr code" />
+        <img src={getQR()} alt="Unique qr code" width={222} height={222}/>
       </div>
     </div>
   )
